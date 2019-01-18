35c2fcc591cdf6129eabe3d56c60d265162d3192
Dominic Classen <dominic.classen@coduct.com>
2019-01-18 12:13:25 +0100

    linting

--- src/Components/Heading/index.tsx
+++ src/Components/Heading/index.tsx
@@ -1,24 +1,24 @@
 //#region Global Imports
-import * as React from 'react';
+import * as React from "react";
 //#endregion Global Imports
 
 //#region Local Imports
-import './style.scss';
+import "./style.scss";
 //#endregion Local Imports
 
 //#region Interface Imports
-import { IHeading } from '@Interfaces';
+import { IHeading } from "@Interfaces";
 //#endregion Interface Imports
 
 export class Heading extends React.Component<IHeading.IProps, IHeading.IState> {
   public render(): JSX.Element {
-	const { text } = this.props;
+    const { text } = this.props;
 
-	return (
-		<div className={'title'}>
-		<span className="title__back">{text}</span>
-		<span className="title__front">{text}</span>
-		</div>
-	);
+    return (
+      <div className={"title"}>
+        <span className="title__back">{text}</span>
+        <span className="title__front">{text}</span>
+      </div>
+    );
   }
 }
